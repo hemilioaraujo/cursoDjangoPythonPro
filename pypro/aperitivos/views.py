cdf7f6673bad4e941305acad5c3da24cfd58bb28
HLAM <hemilioaraujo@gmail.com>
2021-01-20 20:14:35 -0300

    método retorna título conforme slug

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 498736189}
+    videos = {
+        'motivacao': {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 498736189},
+        'instalacao-windows': {'titulo': '', 'vimeo_id': 498736190},
+    }
+    video = videos[slug]
+
+
     return render(request, 'aperitivos/video.html', context={'video': video})
